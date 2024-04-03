01ca8e06f76f7ea00cebc878860021b86989123e
Darek <dareczek06@gmail.com>
2024-04-03 16:35:46 +0200

    Added eleventy settings

--- FAQ-component/.eleventy.js
+++ FAQ-component/.eleventy.js
@@ -1,16 +1,17 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.setBrowserSyncConfig({
-      files: './public/static/**/*.css',
+        files: './public/static/**/*.css',
     });
-  
+    eleventyConfig.addPassthroughCopy("src/assets/");
+
     return {
-      dir: {
-        input: 'src',
-        output: 'public',
-      },
-      templateFormats: ['md', 'njk', 'html'],
-      markdownTemplateEngine: 'njk',
-      htmlTemplateEngine: 'njk',
-      dataTemplateEngine: 'njk',
+        dir: {
+            input: 'src',
+            output: 'public',
+        },
+        templateFormats: ['md', 'njk', 'html'],
+        markdownTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
     };
-  };
+};
