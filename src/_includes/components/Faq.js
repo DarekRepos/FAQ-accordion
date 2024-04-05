fe685437c0a2cbe5e8681c7857872dcce40787b0
Darek <dareczek06@gmail.com>
2024-04-05 23:33:46 +0200

    Added faq component

--- src/_includes/components/Faq.js
+++ src/_includes/components/Faq.js
@@ -6,7 +6,6 @@ function Faq({ question, answer }) {
         <summary>${question}</summary>
         <div>
         <p>${answer}</p>
-        <i aria-hidden="true" class=""></i>
         </div>  
       </details>
   `;
